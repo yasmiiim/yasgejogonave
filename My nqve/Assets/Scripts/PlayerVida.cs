1bbc416dc45f9a981928531fa0aff3fa7a80e752
yasmim <suzzyasmim2016@gmail.com>
2023-11-29 23:16:33 -0300

    animaçao player

--- My nqve/Assets/Scripts/PlayerVida.cs
+++ My nqve/Assets/Scripts/PlayerVida.cs
@@ -7,10 +7,17 @@ public class PlayerVida : MonoBehaviour
     public int vidaMax;
 
     public int vidaAtual;
+    
+    
+    [SerializeField] private Animator animator;
+    private bool isDead = false;
+    
+    private NaveMovi playerMovimento;
     // Start is called before the first frame update
     void Start()
     {
         vidaAtual = vidaMax;
+        playerMovimento = GetComponent<NaveMovi>(); 
     }
 
     // Update is called once per frame
@@ -25,7 +32,19 @@ public class PlayerVida : MonoBehaviour
 
         if (vidaAtual <=0)
         {
-            Destroy(gameObject);
+            Die(); 
         }
+    }
+    
+    void Die()
+    {
+        isDead = true;
+        animator.SetTrigger("playerdie");
+        if (playerMovimento != null)
+        {
+            playerMovimento.enabled = false;
+        }
+
+        Destroy(gameObject, 1f);
     }
 }
