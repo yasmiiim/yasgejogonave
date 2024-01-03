fb4326f2d23c6f64de389e7f5225f0d893a5301f
yasmim <suzzyasmim2016@gmail.com>
2024-01-02 21:09:55 -0300

    ajeitei

--- My nqve/Assets/Scripts/Gerador.cs
+++ My nqve/Assets/Scripts/Gerador.cs
@@ -4,37 +4,36 @@ using UnityEngine;
 
 public class Gerador : MonoBehaviour
 {
-
-    public GameObject[] paraSpawnar;
+    public GameObject[] inimigosParaSpawnar;
 
     public Transform[] pontosDeSpawn;
 
     public float tempoMaxSpawns;
+    private float tempoAtualSpawns;
 
-    public float tempoAtualSpawns;
-    // Start is called before the first frame update
+    
     void Start()
     {
         tempoAtualSpawns = tempoMaxSpawns;
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         tempoAtualSpawns -= Time.deltaTime;
 
         if (tempoAtualSpawns <= 0)
         {
-            Spawnar();
+            SpawnarInimigo(); 
         }
     }
 
-    private void Spawnar()
+    private void SpawnarInimigo()
     {
-        int ObjAleatorio = Random.Range(0, paraSpawnar.Length);
-        int ptsAleatorio = Random.Range(0, pontosDeSpawn.Length);
+        int inimigoAleatorio = Random.Range(0, inimigosParaSpawnar.Length);
+        int pontoAleatorio = Random.Range(0, pontosDeSpawn.Length);
 
-        Instantiate(paraSpawnar[ObjAleatorio], pontosDeSpawn[ptsAleatorio].position, Quaternion.Euler(0f, 0f, -90f));
+        Instantiate(inimigosParaSpawnar[inimigoAleatorio], pontosDeSpawn[pontoAleatorio].position, Quaternion.identity);
         tempoAtualSpawns = tempoMaxSpawns;
     }
 }
